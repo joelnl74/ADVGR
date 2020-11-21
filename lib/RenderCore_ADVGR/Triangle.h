beca9a444b1b6702f4c0ae174f5c0d330bf871af
Joey <joelnl74@gmail.com>
2020-11-21 20:11:51 +0100

    Latest stuff

--- lib/RenderCore_ADVGR/Triangle.h
+++ lib/RenderCore_ADVGR/Triangle.h
@@ -1,16 +1,17 @@
 #pragma once
-#include "Primitive.h"
 #include "core_api_base.h"
-class Triangle : public Primitive
+
+class Triangle
 {
 public:
 	Triangle(float3 p1, float3 p2, float3 p3) : point1(p1), point2(p2), point3(p3) {};
 	Triangle() {};
 
-	bool Intersect(Ray ray) override;
-
 public:
 	float3 point1;
 	float3 point2;
 	float3 point3;
+
+	float3 m_color;
+	int index;
 };
