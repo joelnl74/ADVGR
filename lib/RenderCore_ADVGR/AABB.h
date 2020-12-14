8be43ed0e9b905454d2da983ac8d6f70d32bf335
Joey <joelnl74@gmail.com>
2020-12-15 00:06:59 +0100

    overloader

--- lib/RenderCore_ADVGR/AABB.h
+++ lib/RenderCore_ADVGR/AABB.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "core_api_base.h"
+#include <iostream>
 
 using namespace lighthouse2;
 
@@ -12,6 +13,12 @@ public:
 		minBounds = min;
 		maxBounds = max;
 	};
+
+	bool operator==(const AABB& lhs) const
+	{
+		return lhs.minBounds.x == minBounds.x && lhs.minBounds.y == minBounds.y && lhs.minBounds.z == minBounds.z &&
+			lhs.maxBounds.x == maxBounds.x && lhs.maxBounds.y == maxBounds.y && lhs.maxBounds.z == maxBounds.z;
+	}
 
 	float3 minBounds = make_float3(INT_MAX);
 	float3 maxBounds = make_float3(INT_MIN);
