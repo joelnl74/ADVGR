4f546860802b4f91708886fc0381b2a0c9440e6b
Joey <joelnl74@gmail.com>
2021-02-02 17:15:30 +0100

    Private

--- lib/RenderCore_ADVGR/PhotonMapping.h
+++ lib/RenderCore_ADVGR/PhotonMapping.h
@@ -10,7 +10,6 @@ public:
 	PhotonMapping() {};
 	void Init(float3& position, float3& intensity, int number_of_photons, vector<CoreMaterial> &materials, BVHNode *root, vector<Sphere> &spheres);
 	float3 GatherPhotonEnergy(float3& position, float3& normal, int index);
-	float3 PhotonTrace(Ray &ray, int depth);
 
 	void AddCaustic(int index, Photon& photon);
 	void AddPhoton(int index, Photon& photon);
@@ -20,14 +19,16 @@ public:
 	void AddPhotonVector();
 	void AddShadowPhotonVector();
 private:
-	vector<vector<Photon>> m_PhotonsOnObject = {};
-	vector<vector<Photon>> m_CausticsOnObject = {};
-	vector<vector<Photon>> m_ShadowPhotonsOnObject = {};
-
 	tuple<CoreTri, float, float3, CoreMaterial> Intersect(Ray& ray);
 	float3 Reflect(float3& in, float3 normal);
 	float3 Refract(float3& in, float3& normal, float ior);
+	float3 PhotonTrace(Ray& ray, int depth);
 	float Fresnel(float3& in, float3& normal, float ior);
+
+private:
+	vector<vector<Photon>> m_PhotonsOnObject = {};
+	vector<vector<Photon>> m_CausticsOnObject = {};
+	vector<vector<Photon>> m_ShadowPhotonsOnObject = {};
 
 	int maxDepth = 3;
 	bool caustic;
